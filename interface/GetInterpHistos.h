6fe4975d787d55d0e8c5bdc461b8bc3c4c76dafe
Evan Coleman <evan_a_coleman@brown.edu>
2015-08-13 13:42:26 -0500

    Cleaned up magic in getInterpHistos

---
+++ interface/GetInterpHistos.h
@@ -0,0 +1,26 @@
+#ifndef GetInterpHistos_h
+#define GetInterpHistos_h
+
+#include <TFile.h>
+#include <TH1D.h>
+#include <TString.h>
+
+class GetInterpHistos {
+
+  TString nomLocation = TString("treedir/1xSample/MC8TeV_TTJets_MSDecays_172v5.root");
+  TString maxLocation = TString("treedir/5xSample/MC8TeV_TTJets_widthx5.root");
+  TString outFileLocation = TString("treedir/TMassWeightHistograms.root");
+
+  std::vector<TString> leps = { "E", "EE", "EM", "MM", "M" };
+
+  float maxWidth = 7.5;
+  float nomWidth = 1.5;
+  int interpolations = 3;
+
+public:
+  GetInterpHistos(TString, float, TString, float, int, TString);
+  virtual ~GetInterpHistos() {}
+
+};
+
+#endif
